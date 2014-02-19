f586dbf2bf961c5fe597cef5d660edff9d2ceb13
Deiwin <deiwin.sarjas@cgi.com>
2014-02-19 10:54:04 +0200

    Definitions saved to file

---
+++ kindle-highlights.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/ruby
+
+require 'kindle_highlights'
+require 'yaml'
+require 'net/http'
+require 'xmlsimple'
+%w(rubygems wordnik).each {|lib| require lib}
+
+Wordnik.configure do |config|
+      config.api_key = '<API-Key>'
+end
+
+passwords = File.open('.pw', 'r')
+line = passwords.gets.chomp
+passwords.close
+
+# pass in your Amazon credentials. Loads your books (not highlights) on init, so might take a while                                                             
+kindle = KindleHighlights::Client.new("<email>", line) 
+
+ca = kindle.books.select {|k| kindle.books[k].include? "Cloud Atlas"}
+
+# Get the key of the first book
+book_hash = ca.first.first
+
+highlights = kindle.highlights_for(book_hash)
+
+# Load the file.
+saved_words = YAML.load_stream(File.open('saved_words.yaml'))
+
+puts saved_words.inspect
+# Add a new key-value pair to the root of the first document.
+if saved_words.empty? || saved_words[0].nil?
+  saved_words[0] = []
+end
+# name -> def
+ws = saved_words[0].map{|w| w[:name] }
+
+def cleanup(s)
+  s.downcase.gsub /[\s,.:]/, ''
+end
+
+highlights.map!{|h| {:name => cleanup(h['highlight'])} }
+
+highlights.keep_if do |h|
+  ! ws.include? h[:name]
+end
+
+new_defs = highlights.map do |h|
+  wdef = "Definitions:\n"
+  Wordnik.word.get_definitions(h[:name]).each do |definition|
+    wdef += " - " + definition['text'] + "\n"
+  end
+  wdef += "\nExamples:\n"
+  Wordnik.word.get_examples(h[:name])['examples'].each do |example|
+    wdef += " - " + example['text'] + "\n"
+  end
+  wdef += "\nSynonyms:\n"
+  wdef += " - " + Wordnik.word.get_related(h[:name], :type => 'synonym')[0]['words'].join(', ')
+  puts wdef
+  {:name => h[:name], :def => wdef}
+end
+
+unless new_defs.empty?
+  new_saved_words = saved_words[0] + new_defs
+  File.open('saved_words.yaml', 'w') do |file|
+    file.write(YAML.dump(new_saved_words))
+  end
+end
+
