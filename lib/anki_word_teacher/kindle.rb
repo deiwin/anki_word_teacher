80914625486e87553c8070967a60954af906b51e
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-03-01 18:59:47 +0200

    fix kindle initialization
    uses the new kindle-your-highlights gem

--- lib/anki_word_teacher/kindle.rb
+++ lib/anki_word_teacher/kindle.rb
@@ -1,15 +1,10 @@
-require 'kindle_highlights'
-require 'yaml'
-require 'csv'
-require 'net/http'
-
 module AnkiWordTeacher
   module KindleWords
     class Client
-  
-      BOOKS = AnkiWordTeacher.configuration.kindle_books
-      EMAIL = AnkiWordTeacher.configuration.kindle_email
-      PASS = AnkiWordTeacher.configuration.kindle_password
+      require 'kindle-your-highlights'
+      require 'yaml'
+      require 'csv'
+      require 'net/http'
   
       def initialize(logger = ->(s){puts s})
         @logger = logger
@@ -20,21 +15,24 @@ module AnkiWordTeacher
   
       def initKindle
         # pass in your Amazon credentials. Loads your books (not highlights) on init, so might take a while                                                             
-        @kindle = KindleHighlights::Client.new(EMAIL, PASS)
+        @kindle = KindleHighlights.new(AnkiWordTeacher.configuration.kindle_email, AnkiWordTeacher.configuration.kindle_password) do | h |
+          if h.books.last
+            @logger.call "loading... [#{h.books.last.title}] - #{h.books.last.last_update}"
+          end
+        end
       end
   
       def getWords
         @logger.call "Start fetching Kindle highlights"
-        BOOKS.map do |book_name|
-          @logger.call "Fetching highlights for book " + book_name
-          book = @kindle.books.select {|k| @kindle.books[k].include? book_name}
-          # Get the key of the first book
-          book_hash = book.first.first
-          tags = ["kindle", book_name.downcase.gsub(/\s/,'-')]
-          @kindle.highlights_for(book_hash).map do |h|
-            {:word => h['highlight'], :tags => tags}
+        @kindle.list.highlights.map do |highlight|
+          # Less than 3 words
+          if highlight.content.split.size <= 2
+            tags = ["kindle", highlight.title.downcase.gsub(/\s/,'-')]
+            {:word => highlight.content, :tags => tags}
+          else
+            next
           end
-        end.flatten
+        end.compact
       end
     end
   end
