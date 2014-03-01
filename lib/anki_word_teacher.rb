40912c48b4b0527976ede5bff7dfe751c0d29257
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-03-01 15:15:52 +0200

    Move configurations to a single file
    delete unused evernote_config file

--- lib/anki_word_teacher.rb
+++ lib/anki_word_teacher.rb
@@ -1,1 +1,17 @@
-require 'anki-word-teacher/anki-updater'
+require 'anki_word_teacher/anki_updater'
+require 'anki_word_teacher/configuration'
+require 'logger'
+
+module AnkiWordTeacher
+  class << self
+    attr_accessor: configuration, logger
+    def configure(&block)
+      self.configuration ||= Configuration.new
+      yield (configuration)
+      self.logger ||= configuration.logger
+
+      if !configuration.wordnik_api_key
+        raise "No Wordnik API key set."
+      end
+    end
+end
---
+++ lib/anki_word_teacher/configuration.rb
@@ -0,0 +1,22 @@
+require 'logger'
+
+module AnkiWordTeacher
+  class Configuration
+    attr_accessor: import_file, mappings_file, saved_words_file
+    attr_accessor: wordnik_api_key
+    attr_accessor: evernote_auth_token, evernote_filter
+    attr_accessor: logger
+    attr_accessor: kindle_email, kindle_password, kindle_books
+
+    def initialize
+      @logger = Logger.new(STDOUT)
+
+      @import_file = "import.csv"
+      @mappings_file = "mappings.yaml"
+      @saved_words_file = "saved_words.yaml"
+
+      @evernote_filter = "tag:#Word"
+    end
+
+  end
+end
